bbde428e60c5c256204c39229efee3d97b74374c
Lukas <77892550+fraglet@users.noreply.github.com>
2021-05-05 17:43:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,17 +11,18 @@ from time import sleep
 # Create connection to MySQL Server
 def create_server_connection(host_name, user_name, user_password, database):
     connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=database,
-            auth_plugin='caching_sha2_password'
-        )
-        print("MySQL Database connection successful")
-    except Error as err:
-        print(f"Error: '{err}'")
+    while connection == None:
+        try:
+            connection = mysql.connector.connect(
+                host=host_name,
+                user=user_name,
+                passwd=user_password,
+                database=database,
+                auth_plugin='caching_sha2_password'
+            )
+            print("MySQL Database connection successful")
+        except Error as err:
+            print(f"Error: '{err}'")
 
     return connection
 
